3b5a5e450e64ef96d472778e4f32f66a75e2f052
Inori <andreacremades94@gmail.com>
2020-06-04 12:34:30 +0200

    g

--- Lycoris/Assets/Scripts/Habilities.cs
+++ Lycoris/Assets/Scripts/Habilities.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Habilities : MonoBehaviour
 {
+
+    [SerializeField] float walkSpeed;
+    [SerializeField] float jumpForce;
+
+    Animator animator;
+    CryptoAPITransform transform;
+
+    public enum skeletonStates {  idle, walk, jumping, falling, transf, transfreverse, liana, death };
+    // 0,     1,     2,       3,      4,          5,         6,     7
+
+    const string STATE_IDLE_NAME = "Static";
+    enum Directions {  left, right, max_dir };
+
     // Start is called before the first frame update
     void Start()
     {
